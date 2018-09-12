ca07efc22e14ef8752e58c17f5affa89cb1eb288
Ruchitesh Malukani <41920841+RuchiteshPM@users.noreply.github.com>
2018-09-12 11:11:10 +0530

    Add note functionality added.

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,9 +1,20 @@
-$('li').click(function(){
+$('ul').on('click','li',function(){
 $(this).toggleClass('checkit');
 });
-$('span').click(function(event){
+$('ul').on('click','span',function(event){
 $(this).parent().fadeOut(500,function(){
 	$(this).remove();
 });
 event.stopPropagation();
+});
+$("input[type='text']").keypress(function(event){
+if(event.which===13){//Enter key pressed!
+	
+	//catch the text
+	var Todotxt=$(this).val();
+	//Clear the input box
+	$(this).val("");
+	//append the new data
+	$("ul").append("<li><span>X</span> " + Todotxt + "</li>");
+}
 });
